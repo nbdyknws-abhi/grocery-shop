8d62fbf86a6a8c442f1343835e4541f51c89a7c9
Arpan Chakraborty <chakrabartyarpan8@gmail.com>
2025-09-12 15:41:22 +0530

    adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { registerAdmin, loginAdmin, getAllUsers, deleteUser, updateUser,getAllOrders } from "../controllers/adminController.js";
+import { loginAdmin, getAllUsers, deleteUser, updateUser,getAllOrders } from "../controllers/adminController.js";
 import { adminOnly } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerAdmin);
+//router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
 
 // Protected routes
